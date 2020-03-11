1b280f4e0f7e99b5d4f5e6f983d604c14b521f64
Santiago Matalonga <35964418+smatalonga@users.noreply.github.com>
2020-03-11 09:00:09 +0000

    Update StrawberryQte.java

--- uni_api/uni_api/StrawberryQte.java
+++ uni_api/uni_api/StrawberryQte.java
@@ -9,7 +9,7 @@ public class StrawberryQte extends Quote {
 
 	public StrawberryQte(String council, int hectares, int vph) {
 		//Santi: Wonderig what happens when wrog set of [council, hectares, vph] is sent to this consutructor
-		zone = new Zones().zoneChecker(council);
+		zone = new Zones().zoneChecker(council);//see comment in class Zone.. you are using this as if it were static (not dependent in any state)
 		calcPremium = RatingEngine.getPremium(zone, hectares, vph);
 	}
 
