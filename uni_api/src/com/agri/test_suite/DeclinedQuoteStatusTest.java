101cd828a02723d190e4671e083fd15a69dd04bc
Kyle Kennedy <B00360997@studentmail.uws.ac.uk>
2020-04-02 12:35:17 +0100

    unit test

--- uni_api/src/com/agri/test_suite/DeclinedQuoteStatusTest.java
+++ uni_api/src/com/agri/test_suite/DeclinedQuoteStatusTest.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.Test;
 
 import com.agri.business.RiskData;
 import com.agri.exceptions.ActionException;
+import com.agri.exceptions.PermissionException;
 import com.agri.quote.AgriInsuranceFactory;
 import com.agri.quote.AgriQuote;
+import com.agri.users.Default;
 import com.agri.users.Supervisor;
 import com.agri.users.SupervisorManager;
 import com.agri.users.User;
@@ -19,8 +21,8 @@ class DeclinedQuoteStatusTest {
     int hectares = 500;
     int vph = 100;
     
-    @Test // Supervisor Accept Quote Exception Test
-	void SupAcceptExceptionTest() throws Exception {
+    @Test // Accept Quote Test
+	void acceptQuoteTest() throws Exception {
 		
 		String actual; 
 		String expected;
@@ -29,16 +31,16 @@ class DeclinedQuoteStatusTest {
 		RiskData riskdata = new RiskData(crop, council, hectares, vph);
         AgriQuote quote = AgriInsuranceFactory.createNewQuote(user , riskdata);
         
-        	quote.saveQuote(user);
-        	quote.declineQuote(user);
-        	quote.acceptQuote(user);
-        	actual = quote.getStatus().toString();
-        	expected = "AcceptedQuoteStatus";
-        	assertTrue(actual.contains(expected));
+       	quote.saveQuote(user);
+        quote.declineQuote(user);
+        quote.acceptQuote(user);
+        actual = quote.getStatus().toString();
+        expected = "AcceptedQuoteStatus";
+        assertTrue(actual.contains(expected));
 	}
     
-    @Test // Supervisor Decline Quote Exception Test
-    void SupDeclineExceptionTest() throws Exception {
+    @Test // Decline Quote Exception Test
+    void declineExceptionTest() throws Exception {
 		
         User user = new Supervisor();
         RiskData riskdata = new RiskData(crop, council, hectares, vph);
@@ -46,40 +48,39 @@ class DeclinedQuoteStatusTest {
         
         try {
         	
-            quote.saveQuote(user);
-    		quote.declineQuote(user);
-    	    quote.declineQuote(user);
+	            quote.saveQuote(user);
+	    		quote.declineQuote(user);
+	    		quote.declineQuote(user);
     	    
-            } 
-            
-            catch(ActionException e) {
-              assertEquals(e.getMessage(), ("your quote is already declined"));	
+            } catch (ActionException e) {
+            	
+            	assertEquals(e.getMessage(), ("your quote is already declined"));
+              
             }
     }
     
-    @Test // Supervisor Save Quote Exception Test
-    void SupSavedExceptionTest() throws Exception {
+    @Test // Save Quote Exception Test
+    void savedQuoteExceptionTest() throws Exception {
 		
 	     User user = new Supervisor();
 	     RiskData riskdata = new RiskData(crop, council, hectares, vph);
 	     AgriQuote quote = AgriInsuranceFactory.createNewQuote(user , riskdata); 
 	        
-	        try {
+	     try {
 	        	
-	        quote.saveQuote(user);
-			quote.declineQuote(user);
-		    quote.saveQuote(user);
+	    	 quote.saveQuote(user);
+	    	 quote.declineQuote(user);
+	    	 quote.saveQuote(user);
 
-		    
-	        } 
-	        
-	        catch(ActionException e) {
-	          assertEquals(e.getMessage(), ("your quote is already saved"));	
-	        }
+		  } catch (ActionException e) {
+			  
+			  assertEquals(e.getMessage(), ("your quote is already saved"));
+	          
+	      }
 	}
     
-    @Test // Supervisor Refer Exception Test 
-    void SupReferExceptionTest() throws Exception {
+    @Test // Refer Quote Exception Test 
+    void referQuoteExceptionTest() throws Exception {
     	
     	User user = new Supervisor();
     	RiskData riskdata = new RiskData(crop, council, hectares, vph);
@@ -91,73 +92,17 @@ class DeclinedQuoteStatusTest {
 			quote.declineQuote(user);
 		    quote.referQuote(user);
 		    
-	        } 
+	    } catch (ActionException e) {
+	    	
+	        assertEquals(e.getMessage(), ("you do not have permission to perform that action"));
 	        
-	        catch(ActionException e) {
-	          assertEquals(e.getMessage(), ("you do not have permission to perform that action"));	
-	        }
+	    }
 	}
     
-    @Test // Supervisor Manager Accept Quote Exception Test 
-	void SupManAcceptExceptionTest() throws Exception {
-		
-		String actual; 
-		String expected;
-        
-		User user = new SupervisorManager();
-		RiskData riskdata = new RiskData(crop, council, hectares, vph);
-        AgriQuote quote = AgriInsuranceFactory.createNewQuote(user , riskdata);
-        
-        	quote.saveQuote(user);
-        	quote.declineQuote(user);
-        	quote.acceptQuote(user);
-        	actual = quote.getStatus().toString();
-        	expected = "AcceptedQuoteStatus";
-        	assertTrue(actual.contains(expected));
-	}
     
-    @Test // Supervisor Manager Decline Quote Exception Test 
-    void SupManDeclineExceptionTest() throws Exception {
-		
-        User user = new SupervisorManager();
-        RiskData riskdata = new RiskData(crop, council, hectares, vph);
-        AgriQuote quote = AgriInsuranceFactory.createNewQuote(user , riskdata); 
-        
-        try {
-        	
-            quote.saveQuote(user);
-    		quote.declineQuote(user);
-    	    quote.declineQuote(user);
-    	    
-            } 
-            
-            catch(ActionException e) {
-              assertEquals(e.getMessage(), ("your quote is already declined"));	
-            }
-    }
-    
-    @Test // Supervisor Manager Save Quote Exception Test 
-    void SupManSaveExceptionTest() throws Exception {
+    @Test //accept quote without correct permission level
+    void acceptWrongPermissionTest() throws Exception {
     	
-    	User user = new SupervisorManager();
-    	RiskData riskdata = new RiskData(crop, council, hectares, vph);
-        AgriQuote quote = AgriInsuranceFactory.createNewQuote(user , riskdata); 
-        
-        try {
-        	
-            quote.saveQuote(user);
-    		quote.declineQuote(user);
-    	    quote.saveQuote(user);
-    	    
-            } 
-            
-            catch(ActionException e) {
-              assertEquals(e.getMessage(), ("your quote is already saved"));	
-            }
-    }
-        
-    @Test // Supervisor Manager Refer Quote Exception Test 
-    void SupManReferExceptionTest() throws Exception {
     	User user = new SupervisorManager();
     	RiskData riskdata = new RiskData(crop, council, hectares, vph);
 	    AgriQuote quote = AgriInsuranceFactory.createNewQuote(user , riskdata);
@@ -166,16 +111,34 @@ class DeclinedQuoteStatusTest {
         	
 	        quote.saveQuote(user);
 			quote.declineQuote(user);
-		    quote.referQuote(user);
+			User userDefault = new Default();
+		    quote.acceptQuote(userDefault);
 		    
-	        } 
-	        
-	        catch(ActionException e) {
-	          assertEquals(e.getMessage(), ("you do not have permission to perform that action"));	
-	        }
-    	  
+	     } catch (PermissionException e) {
+	    	 
+	        assertEquals(e.getMessage(), ("you do not have permission to perform that action"));
+	          
+	     } 
     }
     
-     
+    @Test //refer quote without correct permission level
+    void referWrongPermissionTest() throws Exception {
+    	User user = new SupervisorManager();
+    	RiskData riskdata = new RiskData(crop, council, hectares, vph);
+	    AgriQuote quote = AgriInsuranceFactory.createNewQuote(user , riskdata);
+    	
+	    try {
+        	
+	        quote.saveQuote(user);
+			quote.declineQuote(user);
+			User userDefault = new Default();
+		    quote.referQuote(userDefault);
+		    
+	    } catch (PermissionException e) {
+	    	
+	    	assertEquals(e.getMessage(), ("you do not have permission to perform that action"));
+	          
+	    }	  
+    }
 }
     
--- uni_api/src/com/agri/test_suite/SupervisorManagerPermTest.java
+++ uni_api/src/com/agri/test_suite/SupervisorManagerPermTest.java
@@ -1,10 +1,7 @@
 package com.agri.test_suite;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
-
-import com.agri.permissions.PermissionsManager;
 import com.agri.users.SupervisorManager;
 
 class SupervisorManagerPermTest {
